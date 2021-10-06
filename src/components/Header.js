8d782c3e937792f8d0becf7717382bac8dfad17c
Bryan Moore <bryanmoo92@gmail.com>
2021-10-05 21:08:02 -0700

    Removing help page from header:

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <NavLink to='/' activeClassName='is-active' exact={true}>Go Home</NavLink>
-        <NavLink to='/help' activeClassName='is-active'>Get Help</NavLink>
         <NavLink to='/create' activeClassName='is-active'>Create Expense</NavLink>
     </header>
 );
 
+//<NavLink to='/help' activeClassName='is-active'>Get Help</NavLink>
 export default Header;
