1b90d0badf949bf7755cbe9367149e7d6b86300a
Bryan Moore <bryanmoo92@gmail.com>
2021-10-07 12:35:32 -0700

    adding all the styles

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -13,10 +13,16 @@ export class AddExpensePage extends React.Component {
     render() {
         return (
             <div>
-                <h1>Add Expense</h1>
-                <ExpenseForm 
-                    onSubmit={this.onSubmit}
-                />
+                <div className="page-header">
+                    <div className="content-container">
+                        <h1 className="page-header__title">Add Expense</h1>
+                    </div>
+                </div>
+                <div className='content-container'>
+                    <ExpenseForm 
+                        onSubmit={this.onSubmit}
+                    />
+                </div>
             </div>
         );
     }
@@ -27,17 +33,3 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
-
-// const AddExpensePage = (props) => (
-//     <div>
-//         <h1>Add Expense</h1>
-//         <ExpenseForm 
-//             onSubmit={(expense) => {
-//                 props.dispatch(addExpense(expense));
-//                 props.history.push('/');
-//             }}
-//         />
-//     </div>
-// );
-
-// export default connect()(AddExpensePage);
